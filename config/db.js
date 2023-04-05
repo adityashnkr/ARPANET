d3174bb9a6085eba45322340fbef59f9befda9f8
adityashnkr <adityashankar009@gmail.com>
2023-04-05 17:06:26 +0530

    prod changes

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('atlasUri');
+
+const connectDB = async () => {
+	try {
+		mongoose.set('strictQuery', false);
+		await mongoose.connect(db);
+		console.log('MongoDB Connected!');
+	} catch (err) {
+		console.log(err.message);
+		//Exit process with failure
+		process.exit(1);
+	}
+};
+
+module.exports = connectDB;
