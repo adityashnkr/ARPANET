02c438d59d9080fdecc67f8bec448999dc2125c4
Aditya <56380151+adityashnkr@users.noreply.github.com>
2023-10-20 15:52:37 +0000

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+// import './App.css';
 import { Navbar } from './components/layouts/Navbar';
 import { Landing } from './components/layouts/Landing';
 import { Register } from './components/auth/Register';
